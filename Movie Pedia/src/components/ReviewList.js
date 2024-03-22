d8e36f3dcf6561a70279c7337c4e8909e8389ac7
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-03-22 19:10:49 +0900

    sort와 filter 메소드 활용

--- Movie Pedia/src/components/ReviewList.js
+++ Movie Pedia/src/components/ReviewList.js
@@ -5,7 +5,9 @@ function formatDate(value) {
   return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
 }
 
-function ReviewListItem({ item }) {
+function ReviewListItem({ item, onDelete }) {
+  const handleDeleteClick = () => onDelete(item.id);
+
   return (
     <div className="ReviewListItem">
       <img
@@ -18,16 +20,22 @@ function ReviewListItem({ item }) {
         <p>{item.rating}</p>
         <p>{formatDate(item.createdAt)}</p>
         <p>{item.content}</p>
+        <button onClick={handleDeleteClick}>삭제</button>
       </div>
     </div>
   );
 }
 
-function ReviewList({ items }) {
+function ReviewList({ items, onDelete }) {
   return (
     <ul>
       {items.map((item) => {
-        return <li><ReviewListItem item={item}/></li>;
+        return (
+          <li key={item.id}>  
+          {/* 배열의 인덱스는 key 값으로 사용할 수 없음. 반면 id 값은 배열의 상태와 상관없이 항상 같은 데이터를 가리키기 때문에 key 값으로 적합함. */}
+            <ReviewListItem item={item} onDelete={onDelete} />
+          </li>
+        );
       })}
     </ul>
   );
