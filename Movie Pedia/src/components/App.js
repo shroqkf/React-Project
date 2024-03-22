24b43599e625dbc0bd7d8466b165b8d432e840ec
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-03-22 19:11:17 +0900

    sort와 filter 메소드 활용

--- Movie Pedia/src/components/App.js
+++ Movie Pedia/src/components/App.js
@@ -1,10 +1,26 @@
 import ReviewList from "./ReviewList";
-import items from "../mock.json";
+import mockItems from "../mock.json";
+import { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState(mockItems);
+  const [order, setOrder] = useState("createdAt");
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  const handleNewestClick = () => setOrder("creadtedAt");
+  const handleBestClick = () => setOrder("rating");
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => items.id !== id);
+    setItems(nextItems);
+  };
+
   return (
     <div>
-      <ReviewList items={items} />
+      <div>
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleBestClick}>추천순</button>
+      </div>
+      <ReviewList items={sortedItems} onDelete={handleDelete} />
     </div>
   );
 }
