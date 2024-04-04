c9085c783a680a4c0aa0b430bd7b7d128d8d47c1
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-04-04 21:46:13 +0900

    서버에서 정렬된 데이터 받아오기

--- Movie Pedia/src/components/App.js
+++ Movie Pedia/src/components/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ReviewList from "./ReviewList";
 import { getReviews } from "../api";
 
@@ -14,10 +14,15 @@ function App() {
     setItems(nextItems);
   };
 
-  const handleLoadClick = async () => {
-    const { reviews } = await getReviews();
+  const handleLoad = async (orderQuery) => {
+    const { reviews } = await getReviews(orderQuery);
     setItems(reviews);
   };
+
+  useEffect(() => {
+    handleLoad(order);
+  }, [order]);
+
   return (
     <div>
       <div>
@@ -25,7 +30,6 @@ function App() {
         <button onClick={handleBestClick}>추천순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
-      <button onClick={handleLoadClick}>불러오기</button>
     </div>
   );
 }
