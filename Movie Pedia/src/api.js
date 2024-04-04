ea0a3dc01dd4351bc9cf6df6d8b2e52e5f8118f7
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-04-04 21:45:44 +0900

    서버에서 정렬된 데이터를 받아오기

--- Movie Pedia/src/api.js
+++ Movie Pedia/src/api.js
@@ -1,5 +1,21 @@
-export async function getReviews() {
-  const response = await fetch("https://learn.codeit.kr/7517/film-reviews");
+export async function getReviews(order = "createdAt") {
+  const query = `order=${order}`;
+  const response = await fetch(`https://learn.codeit.kr/7517/film-reviews?${query}`);
   const body = await response.json();
   return body;
 }
+
+// const member= {
+//     name: "Alice",
+//     email: "alick@codeitmall.kr",
+//     department: "marketing",
+// };
+
+// fetch("https://learn.codeit.kr/api/members/2",{
+//     method: 'PUT',
+//     body: JSON.stringify(member),
+// })
+//   .then((response) => response.text())
+//   .then((result) => {
+//     console.log(result);
+//   });
