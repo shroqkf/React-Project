a1fb7de19e1c5f40be2f06cca2b1490e202a5efc
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-04-04 21:47:54 +0900

    서버에서 정렬된 데이터 받아오기

---
+++ Food Eat/src/api.js
@@ -0,0 +1,6 @@
+export async function getFoods(order = '') {
+  const query = `order=${order}`;
+  const response = await fetch(`https://learn.codeit.kr/api/foods?${query}`);
+  const body = await response.json();
+  return body;
+}
